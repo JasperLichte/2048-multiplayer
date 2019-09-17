5b4544187ca5f579a4674d73739ea3cbc75e9803
Ricardo Kittmann <Ricardo.Kittmann@Dermalog.com>
2019-09-17 12:02:02 +0200

    Server: fixed gamestatus not being send as string

--- server/Model/Enums/Status.cs
+++ server/Model/Enums/Status.cs
@@ -1,5 +1,9 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace server.model.enums
 {
+  [JsonConverter(typeof(StringEnumConverter))]
     public enum Status
     {
       created = 0,
