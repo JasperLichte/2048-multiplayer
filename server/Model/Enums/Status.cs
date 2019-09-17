7de76dbcceb662e248c7a09d62e7441f78e85e89
Ricardo Kittmann <Ricardo.Kittmann@Dermalog.com>
2019-09-17 13:16:00 +0200

    Server: Changed Status enum to all caps

--- server/Model/Enums/Status.cs
+++ server/Model/Enums/Status.cs
@@ -6,8 +6,8 @@ namespace server.model.enums
   [JsonConverter(typeof(StringEnumConverter))]
     public enum Status
     {
-      created = 0,
-      running = 1,
-      finished=2,  
+      CREATED = 0,
+      RUNNING = 1,
+      FINISHED=2,  
     } 
 }
