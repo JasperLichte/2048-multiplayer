8603a94870ec4f066311fc085b374141cb28a1cd
JasperLichte <contact@jasper-lichte.com>
2019-12-13 12:51:36 +0100

    Client: Add script for deploying client to wwwroot

---
+++ client/deploy/deploy.js
@@ -0,0 +1,27 @@
+const fse = require('fs-extra');
+
+(async ({rootDir, targetDir, dirsToDeploy, filesToDeploy}) => {
+  try {
+    dirsToDeploy.forEach(async dir => {
+      await fse.copy(rootDir + dir, targetDir + dir);
+    });
+
+    filesToDeploy.forEach(async file => {
+      await fse.copy(rootDir + file, targetDir + file);
+    });
+    console.log(
+      `Files are ready to be deployed: ${await fse.realpath(targetDir)}`
+    );
+  } catch (e) {
+    console.log(e);
+  }
+})({
+  rootDir: './',
+  targetDir: '../server/wwwroot/',
+  dirsToDeploy: [
+    'assets',
+  ],
+  filesToDeploy: [
+    'index.html',
+  ],
+});
